029afded8b2e6010c4f002470e23edc35357bc6e
paolo.kahn <paolo.kahn@plantyn.com>
2020-04-13 19:10:36 +0200

    Appsettings for token

--- Discordbot/Discordbot/Program.cs
+++ Discordbot/Discordbot/Program.cs
@@ -2,7 +2,10 @@
 using Discord.Commands;
 using Discord.WebSocket;
 using Discordbot.Commands;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Discordbot
@@ -10,33 +13,54 @@ namespace Discordbot
     public class Program
     {
 		private DiscordSocketClient _client;
+		public static IConfigurationRoot configuration;
 
 		public static void Main(string[] args)
          => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
-			_client = new DiscordSocketClient();
+			ServiceCollection serviceCollection = new ServiceCollection();
+			ConfigureServices(serviceCollection);
 
-			_client.Log += Log;
-			_client.MessageReceived += MessageReceived;
-			await _client.LoginAsync(TokenType.Bot,(""));
-			await _client.StartAsync();
-
-			await new CommandHandler(_client, new CommandService()).InstallCommandsAsync(); //TODO: maybe use DI here? 
-
+			Console.WriteLine(configuration[("DiscordToken")]);
+			CreateBot();
 			// Block this task until the program is closed.
 			await Task.Delay(-1);
 		}
 
-		private async Task MessageReceived(SocketMessage message)
+		public async void CreateBot()
+		{
+			_client = new DiscordSocketClient();
+
+			_client.Log += Log;
+			_client.MessageReceived += PingTester;
+			await _client.LoginAsync(TokenType.Bot, ("MzgwNjcwNjYzNTU3NTEzMjE4.XpSQig.3qOxEAHM3L86nbFrmxtxWgsoBw4"));
+			await _client.StartAsync();
+
+			await new CommandHandler(_client, new CommandService()).InstallCommandsAsync(); //TODO: maybe use DI here? 
+		}
+
+		private static void ConfigureServices(IServiceCollection serviceCollection)
+		{
+			// Build configuration
+			configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+				.AddJsonFile("appsettings.json", false)
+				.Build();
+			// Add access to generic IConfigurationRoot
+			serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
+		}
+
+		//User for development to test if the bot can actually receive requests
+		private async Task PingTester(SocketMessage message)
 		{
 			if (message.Content == "!ping")
 			{
 				await message.Channel.SendMessageAsync("Pong!");
 			}
 		}
-
+		//Also use DI
 		private Task Log(LogMessage msg)
 		{
 			Console.WriteLine(msg.ToString());
