4d80e43c3ab53c494e085f35a732c227b6fe92f5
paolo.kahn <paolo.kahn@plantyn.com>
2020-04-13 18:43:45 +0200

    Basic discord integration

---
+++ Discordbot/Discordbot/Commands/Modules/InfoModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discordbot.Commands.Modules
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        // ~say hello world -> hello world
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+            => ReplyAsync(echo);
+    }
+}
---
+++ Discordbot/Discordbot/Program.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discordbot.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace Discordbot
+{
+    public class Program
+    {
+		private DiscordSocketClient _client;
+
+		public static void Main(string[] args)
+         => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+			_client = new DiscordSocketClient();
+
+			_client.Log += Log;
+			_client.MessageReceived += MessageReceived;
+			await _client.LoginAsync(TokenType.Bot,(""));
+			await _client.StartAsync();
+
+			await new CommandHandler(_client, new CommandService()).InstallCommandsAsync(); //TODO: maybe use DI here? 
+
+			// Block this task until the program is closed.
+			await Task.Delay(-1);
+		}
+
+		private async Task MessageReceived(SocketMessage message)
+		{
+			if (message.Content == "!ping")
+			{
+				await message.Channel.SendMessageAsync("Pong!");
+			}
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
+}
